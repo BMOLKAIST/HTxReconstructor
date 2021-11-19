5828e0f7bc045ce5fa92fe36350455af4698c558
Dohyeon Lee <dleh428@kaist.ac.kr>
2021-11-19 14:33:44 +0900

    refactoring main.m

--- recon3D.m
+++ recon3D.m
@@ -1,59 +1,58 @@
-clear all; clc;
+clear; clc;
 
+% custom variable
+PSFfile='PSF_water_1464_dealias.mat';
+data_dir='C:\Users\labdo\Desktop\20211005.135414.689.height_1mm_chip_bacteria-005';
+
+%% initialization
+% load functions
+head_dir=fileparts(matlab.desktop.editor.getActiveFilename);
+addpath(genpath(fullfile(head_dir,'supplementary_codes')));
 % load PSF
-load('C:\Users\CBC-A1-10\Desktop\matlab\psfMat102.mat'); %psf_water_1464 넣으면 됩니다
+% lambda: mean wavelength of illumination light(unit:um)
+% n_m: RI of media
+% NA_cond: NA of condensor lens
+% NA_obj: NA of objective lens
+% PSF1: Four PSF for each patterns
+%       The size means (# of patterns)x(x axis)x(y axis)x(z axis x 3)
+load(PSFfile);
 
-addpath(genpath('E:\pepsi_code\forDH\forDH\supplementary_codes'));
-cd('D:\CBCData\2021\10\05\20211005.143309.712.bead_test-001\20211005.143309.712.bead_test-001P001'); %tile 모여져있는 folder
-tile_list = dir('2021*');
+%% reconstruction
+cd(data_dir);
+filelist=dir;
+reconstruction_list=filelist(~startsWith({filelist.name},'.'));
 
-for tile_num=1:length(tile_list)
-    disp(['reconstruction in progress ... tile no. ',num2str(tile_num), ' out of ', num2str(length(tile_list))])
-    cd([tile_list(tile_num).name]); 
-    
-    %%% dealiasing example
-        spdir=dir('data3d');
-    if tile_num==1
-        bgdir=dir('bgImages');
-        bg_loaded = loading_stack_notiff_JY2(bgdir(1).folder);
-        bg_loaded = flip_stack(bg_loaded);
-        bg_loaded = bg_loaded(:,:,237:237+1463,:); % image crop
-        
-        sp_loaded = loading_stack_notiff_JY2(spdir(1).folder); % generate (4(pattern) x y z) dimension 
-        sp_loaded = flip_stack(sp_loaded);      % loading sample and background similarly to original version
-        sp_loaded = sp_loaded(:,:,237:237+1463,:);  % image crop
+% find dir containing bgIamges and load the background
+background_dir=dir('*/bgImages/');
+background_loaded=readPepsidata(background_dir(1).folder);
 
-        [RI] = processing_dealias2_JY3(sp_loaded, bg_loaded, PSF1); % scattering potential 
-        Reconimg2 = n_m*sqrt(1-imag(RI));
-        %figure, orthosliceViewer(flipud(Reconimg2),'Scale',[0.162 0.162 0.73]),colormap gray, axis image
-        
-        data1=Reconimg2;
-        TomoData=dir('*.TCF');
-        
-        %save as mat file      
-        data = flipud(data1);
-        savepath = strcat(TomoData.folder,'\',TomoData.name(1:end-4),'_k_included.mat');
-        save(savepath, 'data');
-    else
-        sp_loaded = loading_stack_notiff_JY2(spdir(1).folder);
-        sp_loaded = flip_stack(sp_loaded);      
-        sp_loaded = sp_loaded(:,:,237:237+1463,:); 
+for tile_num=1:length(reconstruction_list)
+    disp(['reconstruction in progress ... tile no. ',num2str(tile_num), ' out of ', num2str(length(reconstruction_list))])
+    recon_dir=reconstruction_list(tile_num);
 
+    %%% dealiasing
+    sample_loaded=readPepsidata([recon_dir.name,'/data3d']);
 
-        [RI] = processing_dealias2_JY3(sp_loaded, bg_loaded, psfOriginal);
-        Reconimg2 = n_m*sqrt(1-imag(RI));
-        
-        data1=Reconimg2;
-        TomoData=dir('*.TCF');
-        
-        %save as mat file
-        data = flipud(data1);
-        savepath = strcat(TomoData.folder,'\',TomoData.name(1:end-4),'_k_included.mat');
-        save(savepath, 'data');
-    end
-    cd ../
+    [RI] = processing_dealias2_JY3(sample_loaded, background_loaded, PSF1); % scattering potential 
+    data = n_m*sqrt(1-imag(RI));
+    %figure, orthosliceViewer(flipud(Reconimg2),'Scale',[0.162 0.162 0.73]),colormap gray, axis image
+
+    %save as mat file
+    data = flipud(data);
+    savepath = strcat(recon_dir.name,'\',recon_dir.name,'_k_included.mat');
+    save(savepath, 'data');
 end
 
+% move back to original location
+cd(head_dir);
+
+%%
+function data = readPepsidata(datadir)
+    assert(isfolder(datadir), 'Given path should be directory');
+    data=loading_stack_notiff_JY2(datadir); % generate (4(pattern) x y z) dimension
+    data=flip_stack(data);          % loading sample and background similarly to original version
+    data=data(:,:,237:237+1463,:);  % crop image
+end
 
     
 
